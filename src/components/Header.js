d9c395e0a6fdd09828602c45120b7d2a55cd7c60
cpclark360 <40728556+cpclark360@users.noreply.github.com>
2019-05-27 21:42:45 -0500

    updated added stop watch

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Stats from './Stats';
+import Stopwatch from './Stopwatch';
 
 // Creating a React stateless component called 'Header'
 const Header = ( props ) => {
@@ -11,6 +12,7 @@ const Header = ( props ) => {
       <header>
         <Stats players={ props.players } />
         <h1>{ props.title }</h1>
+        <Stopwatch />
       </header>
     );
   }
