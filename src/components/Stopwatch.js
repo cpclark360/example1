add8497c6d2c379b4dba7152269d3d30f0395022
cpclark360 <40728556+cpclark360@users.noreply.github.com>
2019-05-28 22:28:15 -0500

    Update Stopwatch.js

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -4,14 +4,35 @@ class Stopwatch extends Component {
     constructor(){
         super()
         this.state = {
-            isRunning: false
+            isRunning: false,
+            elapsedTime: 0,
+            previousTime: 0
+        }
+    }
+
+    
+    tick = ( ) => {
+        if(this.state.isRunning){
+            const now = Date.now();
+            this.setState( prevState => ({
+                previousTime: now,
+                elapsedTime: prevState.elapsedTime + (now - prevState.previousTime)
+            }));   
         }
     }
 
     handleStopwatch = ( ) => {
-        this.setState({
-            isRunning: !this.state.isRunning
-        });
+        this.setState( prevState => ({
+            // !  changes the boolean value to the opposite value ex, from false to true
+            isRunning: !prevState.isRunning,
+        }));
+        if (!this.state.isRunning){
+            this.setState({ previousTime: Date.now()});
+        }
+    }
+
+    componentDidMount() {
+        this.intervalID = setInterval(() => this.tick(), 100);
     }
 
 
@@ -19,7 +40,9 @@ class Stopwatch extends Component {
         return (
             <div className="stopwatch">
                 <h2>Stopwatch</h2>
-                <span className="stopwatch-time">0</span>
+                <span className="stopwatch-time">
+                    {Math.floor(this.state.elapsedTime / 1000)}
+                </span>
                 <button onClick={this.handleStopwatch}>
                     {this.state.isRunning ? 'Stop' : 'Start' }
                 </button>
